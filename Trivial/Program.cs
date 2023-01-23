b9c0459efc1252e5eb6ab3ee7af9279aaf404c92
Adria <a2208022@institutmontilivi.cat>
2023-01-23 17:45:32 +0100

    Agregar archivos de proyecto.

---
+++ Trivial/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo tecla;
+            do
+            {
+                Console.Clear();
+                ShowOptions();
+                tecla = Console.ReadKey();
+                Console.Clear();
+                switch (tecla.Key)
+                {
+                    case ConsoleKey.D1:
+                        Topic1();
+                        break;
+                    case ConsoleKey.D2:
+                        Topic2();
+                        break;
+                    case ConsoleKey.D3:
+                        Topic3();
+                        break;
+                    case ConsoleKey.D0:
+                        MsgNextScreen("PRESS ANY KEY 2 EXIT");
+                        break;
+                }
+            }
+            while (tecla.Key != ConsoleKey.D0);
+        }
+        /// <summary>
+        /// Function to readKey and return to main or exit
+        /// </summary>
+        /// <param name="msg"></param>
+        public static void MsgNextScreen(string msg)
+        {
+            Console.WriteLine(msg);
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// This action shows a menu
+        /// </summary>
+        public static void ShowOptions()
+        {
+            Console.Clear();
+            Console.WriteLine("1 - MATHS TOPIC");
+            Console.WriteLine("2 - GEOGRAPHY");
+            Console.WriteLine("3 - ART");
+            Console.WriteLine("0 - PRESS ANY KEY 2 EXIT");
+        }
+
+        //Marti
+
+        //Adria
+
+        //Somebody
+    }
+}
