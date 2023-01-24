6b948da1bde054ce0d75dbb792f5d0220fb4c2b6
Usuario <Usuario@LAPTOP-SGIM5IV9>
2023-01-24 18:10:12 +0100

    Topic3Comented

--- Trivial/Program.cs
+++ Trivial/Program.cs
@@ -66,7 +66,144 @@ namespace PR01
         }
 
         //Marti
+        //HISTORY 
+        public static string Topic4() 
+        {
+            Console.Clear();
 
+            char abcd, correct;
+            bool valid = false;
+
+            while (!valid)
+            {
+                try
+                {
+                    Console.WriteLine("1- WHEN IS THE FIRST WORLD WAR STARTED?");
+                    Console.WriteLine("a) 1900");
+                    Console.WriteLine("b) 2023");
+                    Console.WriteLine("c) 1910");
+                    Console.WriteLine("d) 1890");
+                    correct = 'c';
+
+                    if (abcd != 'a' && abcd != 'b' && abcd != 'c' && abcd != 'd') throw new Exception("ENTER ONE THE OPTIONS");
+
+                    if (IsCorrectTopic(abcd, correct)) Console.WriteLine("CORRECT!! GOOD JOB");
+                    else Console.WriteLine("WRONG!! NICE TRY");
+
+                    valid = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            while (!valid)
+            {
+                try
+                {
+                    Console.WriteLine("2- WHO IS THE FIRST PRESIDENT OF THE UNITED STATES?");
+                    Console.WriteLine("a) PERRO SANCHEZ");
+                    Console.WriteLine("b) WASHINGTON");
+                    Console.WriteLine("c) OBAMA");
+                    Console.WriteLine("d) TRUMP");
+                    correct = 'b';
+
+                    if (abcd != 'a' && abcd != 'b' && abcd != 'c' && abcd != 'd') throw new Exception("ENTER ONE THE OPTIONS");
+
+                    if (IsCorrectTopic(abcd, correct)) Console.WriteLine("CORRECT!! GOOD JOB");
+                    else Console.WriteLine("WRONG!! NICE TRY");
+
+                    valid = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            while (!valid)
+            {
+                try
+                {
+                    Console.WriteLine("3- WHERE IS NAPOLEON BONAPARTE FROM?");
+                    Console.WriteLine("a) SPAIN");
+                    Console.WriteLine("b) UNITED KINDOM");
+                    Console.WriteLine("c) ITALY");
+                    Console.WriteLine("d) FRANCE");
+                    correct = 'd';
+
+                    if (abcd != 'a' && abcd != 'b' && abcd != 'c' && abcd != 'd') throw new Exception("ENTER ONE THE OPTIONS");
+
+                    if (IsCorrectTopic(abcd, correct)) Console.WriteLine("CORRECT!! GOOD JOB");
+                    else Console.WriteLine("WRONG!! NICE TRY");
+
+                    valid = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            while (!valid)
+            {
+                try
+                {
+                    Console.WriteLine("4- WHERE IS JULIUS CESAR FROM?");
+                    Console.WriteLine("a) ROME");
+                    Console.WriteLine("b) PARIS");
+                    Console.WriteLine("c) MADRID");
+                    Console.WriteLine("d) NOVA YORK");
+                    correct = 'a';
+
+                    if (abcd != 'a' && abcd != 'b' && abcd != 'c' && abcd != 'd') throw new Exception("ENTER ONE THE OPTIONS");
+
+                    if (IsCorrectTopic(abcd, correct)) Console.WriteLine("CORRECT!! GOOD JOB");
+                    else Console.WriteLine("WRONG!! NICE TRY");
+
+                    valid = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            while (!valid)
+            {
+                try
+                {
+                    Console.WriteLine("5- WHEN WAS THE BERLIN WALL REMOVED?");
+                    Console.WriteLine("a) 2000");
+                    Console.WriteLine("b) 1989");
+                    Console.WriteLine("c) 1900");
+                    Console.WriteLine("d) 1970");
+                    correct = 'b';
+
+                    if (abcd != 'a' && abcd != 'b' && abcd != 'c' && abcd != 'd') throw new Exception("ENTER ONE THE OPTIONS");
+
+                    if (IsCorrectTopic(abcd, correct)) Console.WriteLine("CORRECT!! GOOD JOB");
+                    else Console.WriteLine("WRONG!! NICE TRY");
+
+                    valid = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+        //SERIES AND MOVIES
+        public static string Topic5() 
+        {
+            
+        }
+        //FOOD
+        public static string Topic6() 
+        {
+
+        }
+        public static bool IsCorrectTopic(char abcd, char correct)
+        {
+            return abcd == correct;
+        }
 
         //Adria
 
